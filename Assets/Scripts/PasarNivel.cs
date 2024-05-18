f601fcc0e3fcb2b8281fab2fc5a9d7ead28f753e
Nicolás Gamarra <nicolas.gamarra@fing.edu.uy>
2024-05-18 18:06:11 -0300

    Update 3.0

--- Assets/Scripts/PasarNivel.cs
+++ Assets/Scripts/PasarNivel.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PasarNivel : MonoBehaviour
 {
+    [Header("Config")]
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject CameraController;
     [SerializeField] private int PasarANivel;
+    [SerializeField] private int UltimoNivel;
+
+    [Header("Grid")]
+    [SerializeField] private TilemapRenderer Mapa;
+    [SerializeField] private TilemapRenderer Muro;
+    [SerializeField] private TilemapRenderer Techo;
+
     private BoxCollider2D bc;
 
     // Start is called before the first frame update
@@ -19,5 +28,12 @@ public class PasarNivel : MonoBehaviour
     {
         CameraController.gameObject.GetComponent<CamaraController>().ChangeLevel(PasarANivel);
         Player.gameObject.GetComponent<Color_Personaje>().ResetColor();
+
+        if (PasarANivel == UltimoNivel) 
+        {
+            Mapa.material.color = new Color32(0, 255, 255, 255);
+            Muro.material.color = new Color32(0, 255, 255, 255);
+            Techo.material.color = new Color32(0, 255, 255, 255);
+        }
     }
 }
--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -7,14 +7,7 @@ public class Reset : MonoBehaviour
 {
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject CameraController;
-    [SerializeField] private GameObject Azul;
-    [SerializeField] private GameObject Verde;
-    [SerializeField] private GameObject Azul1;
-    [SerializeField] private GameObject Verde1;
-    [SerializeField] private GameObject Azul2;
-    [SerializeField] private GameObject Verde2;
-    [SerializeField] private GameObject Azul3;
-    [SerializeField] private GameObject Verde3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,7 +33,7 @@ public class Reset : MonoBehaviour
                 Player.gameObject.transform.position = new Vector3(19.5163f, 9.268312f, 0);
                 break;
             case 2:
-                Player.gameObject.transform.position = new Vector3(19.51628f, 22.26069f, 0);
+                Player.gameObject.transform.position = new Vector3(19.51628f, 21.26069f, 0);
                 break;
             case 3:
                 Player.gameObject.transform.position = new Vector3(19.51608f, 34.26069f, 0);
